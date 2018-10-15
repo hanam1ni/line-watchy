e20e6ee1fba6bdbdfb8aa6de18007236d3851617
HanaM1ni <immicky.a@gmail.com>
2018-10-15 16:38:25 +0700

    Initialize bot

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import linebot from 'linebot'
+import express from 'express'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+const app = express();
+
+const linebotParser = bot.parser();
+
+app.post('/linewebhook', linebotParser);
+
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+});
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log('LineBot is running.');
+});
